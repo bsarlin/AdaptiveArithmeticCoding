2067cb791fa77c770c5d8abd71ec87f8214f8bae
rfmajor <rfmajor99@gmail.com>
2023-06-16 12:59:41 +0200

    Add comments, move some logic from encoder to number_range

--- src/encoder.py
+++ src/encoder.py
@@ -15,28 +15,13 @@ class Encoder:
         self.number_range = NumberRange(statistic=statistic)
 
     def encode(self, 
-               message_to_encode: str) -> Tuple(float, float):
+               message_to_encode: str) -> Tuple[float, float]:
         lower_bound = 0.0
         upper_bound = 1.0
         for i in range(len(message_to_encode)):
             character = message_to_encode[i]
-            character_info = self.number_range.get_character_information(character=character)
-            if character_info != None:
-                is_new_character = False
-                list_index = character_info["list_index"]
-                temp_lower_bound = character_info["lower_bound"]
-                temp_upper_bound = character_info["upper_bound"]
-            else:
-                is_new_character = True
-                list_index = None
-                temp_lower_bound = None
-                temp_upper_bound = None
             self.number_range.update_information(character=character,
-                                                 count=1,
-                                                 is_new_character=is_new_character,
-                                                 list_index=list_index,
-                                                 lower_bound=temp_lower_bound,
-                                                 upper_bound=temp_upper_bound)
+                                                 count=1)
             self.number_range.update_probabilities()
             self.number_range.update_bounds(lower_bound=lower_bound,
                                             upper_bound=upper_bound)
@@ -59,23 +44,8 @@ class Encoder:
         result = ""
         for i in range(len(message_to_encode)):
             character = message_to_encode[i]
-            character_info = self.number_range.get_character_information(character=character)
-            if character_info != None:
-                is_new_character = False
-                list_index = character_info["list_index"]
-                temp_lower_bound = character_info["lower_bound"]
-                temp_upper_bound = character_info["upper_bound"]
-            else:
-                is_new_character = True
-                list_index = None
-                temp_lower_bound = None
-                temp_upper_bound = None
             self.number_range.update_information(character=character,
-                                                 count=1,
-                                                 is_new_character=is_new_character,
-                                                 list_index=list_index,
-                                                 lower_bound=temp_lower_bound,
-                                                 upper_bound=temp_upper_bound)
+                                                 count=1)
             self.number_range.update_probabilities()
             self.number_range.update_bounds(lower_bound=0.0,
                                             upper_bound=1.0)
